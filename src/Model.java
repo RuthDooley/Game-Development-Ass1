362e4ac408268ab036adddab8032108e2a20e544
Ruth Dooley <ruthdooley@Ruths-MacBook-Pro.local>
2023-02-16 11:57:30 +0000

    cleaning without breaking II

--- src/Model.java
+++ src/Model.java
@@ -37,8 +37,6 @@ public class Model {
 	
 	private GameObject Player;
 	private Controller controller = Controller.getInstance();
-	private CopyOnWriteArrayList<GameObject> EnemiesList  = new CopyOnWriteArrayList<GameObject>();
-	private CopyOnWriteArrayList<GameObject> BulletList  = new CopyOnWriteArrayList<GameObject>();
 	private static CopyOnWriteArrayList<GameObject> LettuceBinList  = new CopyOnWriteArrayList<GameObject>();
 	private static CopyOnWriteArrayList<GameObject> LettuceList  = new CopyOnWriteArrayList<GameObject>();
 	private static CopyOnWriteArrayList<GameObject> BinList  = new CopyOnWriteArrayList<GameObject>();
@@ -75,6 +73,117 @@ public class Model {
 		playerLogic(); 
 	}
 
+	public static int gridSpace;
+	public static String objectPlayerHolding = "none";
+	private void playerLogic() {
+		if(Controller.getInstance().isKeyAPressed()){
+			actionOnDirectionKeyStroke("left");
+		}
+		
+		if(Controller.getInstance().isKeyDPressed()){
+			actionOnDirectionKeyStroke("right");
+		}
+			
+		if(Controller.getInstance().isKeyWPressed()){
+			actionOnDirectionKeyStroke("up");
+		}
+		
+		if(Controller.getInstance().isKeySPressed()){
+			actionOnDirectionKeyStroke("down");
+		}
+		
+		if(Controller.getInstance().isKeySpacePressed()){
+			actionOnSpaceStroke();
+		}
+	}
+
+	private void actionOnDirectionKeyStroke (String direction){
+		//Turn the player
+		turningPlayer(direction);
+
+		//Get the position of the player
+		gridSpace = Point3f.getGridValue(Player.getCentre());
+
+		int spaceMoveInto = 0;
+		switch (direction){
+			case "up":
+				spaceMoveInto = gridSpace - 100;
+				break;
+			case "down":
+				spaceMoveInto = gridSpace + 100;
+				break;
+			case "left":
+				spaceMoveInto = gridSpace - 100_000;
+				break;
+			case "right":
+				spaceMoveInto = gridSpace + 100_000;
+				break;
+			default:
+				System.out.println("Error Model.actionOnDirectionKeyStroke() 1.0");
+		}
+		
+		if (!Point3f.spacesOccupied.contains(spaceMoveInto)){
+			Point3f.removeCollider(gridSpace);
+
+			switch (direction){
+				case "up":
+					Controller.getInstance().setKeyWPressed(false);	
+					Player.getCentre().ApplyVector( new Vector3f(0,widthAndHeight,0));
+					break;
+				case "down":
+					Controller.getInstance().setKeySPressed(false);	
+					Player.getCentre().ApplyVector( new Vector3f(0,-widthAndHeight,0));
+					break;
+				case "left":
+					Controller.getInstance().setKeyAPressed(false);	
+					Player.getCentre().ApplyVector( new Vector3f(-widthAndHeight,0,0));
+					break;
+				case "right":
+					Controller.getInstance().setKeyDPressed(false);	
+					Player.getCentre().ApplyVector( new Vector3f(widthAndHeight,0,0));
+					break;
+				default:
+					System.out.println("Error Model.actionOnDirectionKeyStroke() 1.0");
+			}
+			
+			gridSpace = Point3f.getGridValue(Player.getCentre());
+			Point3f.addCollider(gridSpace);
+		}
+	}
+
+	private void actionOnSpaceStroke(){
+		gridSpace = spaceInfrontOfPlayer();
+
+		if (Point3f.binSpacesOccupied.contains(gridSpace) && objectPlayerHolding != "none"){
+			System.out.println("here this is a bin");
+			objectPlayerHolding = "none";
+		} else if (objectPlayerHolding != "none"){ //If player is holding something ie. lettuce
+			//Implement later can only place down on certain objects and also where there are no other lettuces
+
+			//Add new lettuce poistion to the counter space ie. the space infront of the player 
+			LettuceList.add(new GameObject("res/bullet.png",100,100,Point3f.setPointInit(gridSpace/1000,gridSpace % 1_000, "lettuce")));
+			objectPlayerHolding = "none";
+		} else if (Point3f.lettuceSpacesOccupied.contains(gridSpace)){
+			objectPlayerHolding = "lettuce";
+
+			//Lettuce position removed from the lettuce array and removed from the lettuce collisions
+			for (GameObject temp : LettuceList){
+				if (temp.getCentre().getX() == gridSpace/1000 && temp.getCentre().getY() == gridSpace%1000){
+					LettuceList.remove(temp);
+				}
+			}
+			Point3f.lettuceSpacesOccupied.remove(Point3f.lettuceSpacesOccupied.indexOf(gridSpace));
+			
+			//Change player sprite to player with lettuce
+		} else if (Point3f.lettuceBinSpacesOccupied.contains(gridSpace)){
+			LettuceList.add(new GameObject("res/bullet.png",100,100,Point3f.setPointInit(gridSpace/1000,gridSpace % 1_000, "lettuce")));
+			System.out.println("lettuce list " + LettuceList.get(0).getCentre().getX() + LettuceList.get(0).getCentre().getY());
+			System.out.println("lettuce list cooliders" + Point3f.lettuceSpacesOccupied.toString());
+		}
+
+		Controller.getInstance().setKeySpacePressed(false);
+	}
+
 	public static String directPlayerfacing = "up";
 	private void turningPlayer (String direction){
 		//direction = up, down, left right
@@ -83,102 +192,11 @@ public class Model {
 		Player.setTextureLocation(location);
 	}
 
-	public static int gridSpace;
-	public static String objectPlayerHolding = "none";
-	private void playerLogic() {
-		if(Controller.getInstance().isKeyAPressed()){
-			turningPlayer("left");
-			gridSpace = Point3f.getGridValue(Player.getCentre());
-
-			if (/*gridSpace/100_000 d!= 0 && */!Point3f.spacesOccupied.contains(gridSpace - 100_000)){
-				Point3f.removeCollider(gridSpace);
-				Player.getCentre().ApplyVector( new Vector3f(-widthAndHeight,0,0));
-				Controller.getInstance().setKeyAPressed(false);	
-				gridSpace = Point3f.getGridValue(Player.getCentre());
-				Point3f.addCollider(gridSpace);
-			}
-		}
-		
-		if(Controller.getInstance().isKeyDPressed()){
-			turningPlayer("right");
-			gridSpace = Point3f.getGridValue(Player.getCentre());
-
-			if (/*gridSpace/100_000 != 11 &&*/ !Point3f.spacesOccupied.contains(gridSpace + 100_000)){
-				Point3f.removeCollider(gridSpace);
-				Player.getCentre().ApplyVector( new Vector3f(widthAndHeight,0,0));
-				Controller.getInstance().setKeyDPressed(false);	
-				gridSpace = Point3f.getGridValue(Player.getCentre());
-				Point3f.addCollider(gridSpace);
-			}
-
-		// 	//Could make an else here to do sound effects no move
-		}
-			
-		if(Controller.getInstance().isKeyWPressed()){
-			turningPlayer("up");
-			gridSpace = Point3f.getGridValue(Player.getCentre());
-
-			if (/*gridSpace/100_000 d!= 0 && */!Point3f.spacesOccupied.contains(gridSpace - 100)){
-				Point3f.removeCollider(gridSpace);
-				Player.getCentre().ApplyVector( new Vector3f(0,widthAndHeight,0));
-				Controller.getInstance().setKeyWPressed(false);	
-				gridSpace = Point3f.getGridValue(Player.getCentre());
-				Point3f.addCollider(gridSpace);
-			}
-		}
-		
-		if(Controller.getInstance().isKeySPressed()){
-			turningPlayer("down");
-			gridSpace = Point3f.getGridValue(Player.getCentre());
-
-			if (/*gridSpace/100_000 d!= 0 && */!Point3f.spacesOccupied.contains(gridSpace + 100)){
-				Point3f.removeCollider(gridSpace);
-				Player.getCentre().ApplyVector( new Vector3f(0,-widthAndHeight,0));
-				Controller.getInstance().setKeySPressed(false);	
-				gridSpace = Point3f.getGridValue(Player.getCentre());
-				Point3f.addCollider(gridSpace);
-			}
-		}
-		
-		if(Controller.getInstance().isKeySpacePressed()){
-			gridSpace = spaceInfrontOfPlayer();
-
-			if (Point3f.binSpacesOccupied.contains(gridSpace) && objectPlayerHolding != "none"){
-				System.out.println("here this is a bin");
-				objectPlayerHolding = "none";
-			} else if (objectPlayerHolding != "none"){ //If player is holding something ie. lettuce
-				//Implement later can only place down on certain objects and also where there are no other lettuces
-
-				//Add new lettuce poistion to the counter space ie. the space infront of the player 
-				LettuceList.add(new GameObject("res/bullet.png",100,100,Point3f.setPointInit(gridSpace/1000,gridSpace % 1_000, "lettuce")));
-				objectPlayerHolding = "none";
-			} else if (Point3f.lettuceSpacesOccupied.contains(gridSpace)){
-				objectPlayerHolding = "lettuce";
-
-				//Lettuce position removed from the lettuce array and removed from the lettuce collisions
-				for (GameObject temp : LettuceList){
-					if (temp.getCentre().getX() == gridSpace/1000 && temp.getCentre().getY() == gridSpace%1000){
-						LettuceList.remove(temp);
-					}
-				}
-				Point3f.lettuceSpacesOccupied.remove(Point3f.lettuceSpacesOccupied.indexOf(gridSpace));
-				
-				//Change player sprite to player with lettuce
-			} else if (Point3f.lettuceBinSpacesOccupied.contains(gridSpace)){
-				LettuceList.add(new GameObject("res/bullet.png",100,100,Point3f.setPointInit(gridSpace/1000,gridSpace % 1_000, "lettuce")));
-				System.out.println("lettuce list " + LettuceList.get(0).getCentre().getX() + LettuceList.get(0).getCentre().getY());
-				System.out.println("lettuce list cooliders" + Point3f.lettuceSpacesOccupied.toString());
-			}
-
-			Controller.getInstance().setKeySpacePressed(false);
-		}
-	}
-
 	private int spaceInfrontOfPlayer (){
 		//Find the position of the player
 		gridSpace = Point3f.getGridValue(Player.getCentre());
-		//Adjust to find the space infront of them
 
+		//Adjust to find the space infront of them
 		switch (directPlayerfacing){
 			case "up":
 				gridSpace -= 100;
